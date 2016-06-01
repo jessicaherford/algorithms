110eea2f4eb9c8f339171fb9f2a35825fda9b697
JessicaHerford <jrherford12@gmail.com>
2016-05-31 21:12:50 -0600

    Got it working w/o graph tracer

--- depth-first-search/script.js
+++ depth-first-search/script.js
@@ -1,4 +1,4 @@
-var tracer = new DirectedGraphTracer().attach(new LogTracer());
+// var tracer = new DirectedGraphTracer().attach(new LogTracer());
 var G = [ // G[i][j] indicates whether the path from the i-th node to the j-th node exists or not
     [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
@@ -13,14 +13,16 @@ var G = [ // G[i][j] indicates whether the path from the i-th node to the j-th n
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
-tracer._setTreeData(G, 0);
+// tracer._setTreeData(G, 0);
 
 function DFS(node, parent) { // node = current node, parent = previous node
-    tracer._visit(node, parent)._wait();
+    // tracer._visit(node, parent)._wait();
     for (var i = 0; i < G[node].length; i++) {
         if (G[node][i]) { // if current node has the i-th node as a child
             DFS(i, node); // recursively call DFS
         }
+        console.log(i, node);
     }
+
 }
 DFS(0);
