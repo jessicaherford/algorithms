86f79b1e18927aade22f180644fe94fd636eff13
JessicaHerford <jrherford12@gmail.com>
2016-06-02 10:02:49 -0600

    breadth-first-search

---
+++ breadth-first-search/script.js
@@ -0,0 +1,27 @@
+var G = [ // G[i][j] indicates whether the path from the i-th node to the j-th node exists or not
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+function BFS(s) { // s = start node
+    var Q = [];
+    Q.push(s); // add start node to queue
+    while (Q.length > 0) {
+        var node = Q.shift(); // dequeue
+        for (var i = 0; i < G[node].length; i++) {
+            if (G[node][i]) { // if current node has the i-th node as a child
+                Q.push(i); // add child node to queue
+            }
+        }
+    }
+}
+BFS(0);
